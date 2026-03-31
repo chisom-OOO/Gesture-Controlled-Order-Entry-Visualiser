00fde72ce78c54edf4d8c04cdd22b919b9c55f9f
Chisom <ch.obidimalor@gmail.com>
2026-03-31 13:07:46 +0100

    gesture detection working

--- src/gesture.py
+++ src/gesture.py
@@ -1,9 +1,41 @@
 import cv2
 import mediapipe as mp
+import math
 
 mp_hands = mp.solutions.hands #uses hand module
 mp_draw = mp.solutions.drawing_utils #drawing utility to draw the skeleton
 
+def get_distance(p1, p2):
+    return math.sqrt((p1.x-p2.x)**2 + (p1.y-p2.y)**2)
+#calculate distance between two hand landmarks
+
+def detect_gesture(hand_landmarks):
+    landmarks = hand_landmarks.landmark
+    # storing the landmarks as the hand landmarks (21 points found my mediapipe) so you can refer to each point by index
+
+    thumb_tip= landmarks[4]
+    index_tip= landmarks[8]
+    middle_tip= landmarks[12]
+    ring_tip= landmarks[16]
+    pinky_tip= landmarks[20]
+
+    wrist = landmarks[0]
+
+    pinch_distance = get_distance(thumb_tip, index_tip)
+    if pinch_distance < 0.05:
+        return 'pinch' #pinch detection
+    #calculates if distance is less that 5% of screen width
+    
+    tips = [thumb_tip, index_tip, middle_tip, ring_tip, pinky_tip]
+    distances = [get_distance(tip,wrist) for tip in tips]
+    if all(d>0.27 for d in distances):
+        return 'spread' #spread detection
+    
+    return 'none'
+    # calculate each of the fingertips distance from the central point of the wrist
+    # more than 28% of screen width
+    # all() function returns true if every single distance is over threshhold
+    
 def run_gesture_feed():
     cap = cv2.VideoCapture(0) #0 means use firt web cam (like index positions)
 #cap is a live feed object to read frames from
@@ -28,13 +60,24 @@ def run_gesture_feed():
             result = hands.process(rgb)
         # opencv stores frames in BGR by default, MediaPipe needs RGB
         # converted and passed through the detector
-
+            gesture = 'none'
             if result.multi_hand_landmarks:
                 for hand_landmarks in result.multi_hand_landmarks:
                     mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                    gesture = detect_gesture(hand_landmarks)
                 # result.multi_hand_landmarks contains 21 points MediaPipe found on hand *knucles, fingertips etc
                 # if found a hand, loop through and draw all dots and ligns connecting them
         
+            colour = (255,255,255)
+            if gesture == "pinch":
+                colour = (0,255,0) #green=long
+            elif gesture == "spread":
+                colour = (0,0,255) #red=short
+                #OpenCV colours are BGR not RGB
+
+            cv2.putText(frame, f'Gesture: {gesture.upper()}', (10,40),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, colour, 2)
+            # draws text onto frame
             cv2.imshow("Hand Tracker", frame) #renders frame in new window called hand tracker
             if cv2.waitKey(1) & 0xFF == ord("q"): # wait key waits 1 ms between frames, if Q is pressed loop breaks feed ends
                 break
